1a7b4cab7db4c5400471e36931a38d6f9ba83674
Srinivas R <2441319@cognizant.com>
2025-09-12 14:50:23 +0530

    updated db.json

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './Auth/customer/login/login.component';
 import { SignupComponent } from './Auth/customer/signup/signup.component';
+import { CardComponent } from './Components/card/card.component';
 
 const routes: Routes = [
     {path:'login', component:LoginComponent, outlet:'modal'},
-    {path:'signup', component:SignupComponent, outlet:'modal'}
+    {path:'signup', component:SignupComponent, outlet:'modal'},
+    {path : 'packages', component : CardComponent}
 ];
 
 @NgModule({
@@ -13,5 +15,5 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule {
-
+    
  }
