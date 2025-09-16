2414a79a9df8b9c5d3bc230202d6aef34342eaa0
Stany Romero Stalin <2440862@cognizant.com>
2025-09-16 10:01:45 +0530

    modification in navbar component and module.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,4 @@
-import { MatExpansionModule, MatAccordion,MatExpansionPanel,MatExpansionPanelHeader,MatExpansionPanelTitle } from '@angular/material/expansion';
-import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +13,16 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ListPackagesComponent } from './Components/list-packages/list-packages.component';
 import {  HttpClientModule } from '@angular/common/http';
 import { SearchpipePipe } from './Components/pipes/searchpipe.pipe';
+import { NgModule } from '@angular/core';
+import { MatAccordion, MatExpansionPanel, MatExpansionPanelHeader, MatExpansionPanelTitle, MatExpansionModule } from '@angular/material/expansion';
+import { HomeComponent } from './home/home.component';
+import { PrivacyComponent } from './pages/privacy/privacy.component';
+import { FaqComponent } from './pages/faq/faq.component';
+import { TncComponent } from './pages/tnc/tnc.component';
+import { AdminNavbarComponent } from './Auth/admin/admin-navbar/adminnavbar.component';
+import { AdminProfileComponent } from './Auth/admin/admin-profile/admin-profile.component';
+import { AdminlayoutComponent } from './Auth/admin/adminlayout/adminlayout.component';
+import { CardDetailComponent } from './Components/card-detail/card-detail.component';
 
 
 
@@ -29,6 +38,18 @@ import { SearchpipePipe } from './Components/pipes/searchpipe.pipe';
     SignupComponent,
     ListPackagesComponent,
     SearchpipePipe,
+    DashboardComponent,
+    HomeComponent,
+    PrivacyComponent,
+    FaqComponent,
+    TncComponent,
+    AdminNavbarComponent,
+    AdminProfileComponent,
+    AdminlayoutComponent,
+    CardDetailComponent
+     
+    
+    
   ],
   imports: [
     BrowserModule,
@@ -36,6 +57,11 @@ import { SearchpipePipe } from './Components/pipes/searchpipe.pipe';
     ReactiveFormsModule,
     FormsModule,
     HttpClientModule,
+    MatExpansionModule,
+    MatAccordion,
+    MatExpansionPanel,
+    MatExpansionPanelHeader,
+    MatExpansionPanelTitle
     
   ],
   providers: [],
