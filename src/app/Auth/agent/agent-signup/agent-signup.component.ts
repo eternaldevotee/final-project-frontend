e57bd8b46e512566d26c053232c76c164b922275
Vivek <2440688@cognizant.com>
2025-09-23 16:21:07 +0530

    Agent(login and signup)

--- src/app/Auth/agent/agent-signup/agent-signup.component.ts
+++ src/app/Auth/agent/agent-signup/agent-signup.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { AuthserviceService } from '../../../Services/authservice.service';
 
 @Component({
   selector: 'app-agent-signup',
@@ -8,57 +9,119 @@ import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators }
   styleUrl: './agent-signup.component.css'
 })
 export class AgentSignupComponent {
-signup! : FormGroup;
+// signup! : FormGroup;
  
-  constructor(private fb : FormBuilder){
-    this.signup = this.fb.group({
-      name : ['', [Validators.required, Validators.minLength(3), Validators.pattern(/^[A-Za-z]+(?: [A-Za-z]+)*$/)]],
-      email : ['', [Validators.required, Validators.email]],
-      number : ['', [Validators.required, Validators.pattern(/^[0-9]{10}$/)]], 
-      password: this.fb.group({
-        pwd: ['', [Validators.required, Validators.pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)] ],
-        cpwd: ['', [Validators.required, Validators.minLength(8)]]
-      }, { validators: this.equalPassword }),
-      });
-  }
+//   constructor(private fb : FormBuilder){
+//     this.signup = this.fb.group({
+//       name : ['', [Validators.required, Validators.minLength(3), Validators.pattern(/^[A-Za-z]+(?: [A-Za-z]+)*$/)]],
+//       email : ['', [Validators.required, Validators.email]],
+//       number : ['', [Validators.required, Validators.pattern(/^[0-9]{10}$/)]], 
+//       password: this.fb.group({
+//         pwd: ['', [Validators.required, Validators.pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)] ],
+//         cpwd: ['', [Validators.required, Validators.minLength(8)]]
+//       }, { validators: this.equalPassword }),
+//       });
+//   }
+
+//   get name(){
+//     return this.signup.get(['name']);
+//   }
+  
+//   get email(){
+//     return this.signup.get(['email']);
+//   }
+
+//   get number(){
+//     return this.signup.get(['number']);
+//   }
+
+//   get password(){
+//     return this.signup.get(['password'])
+//   }
+
+//   get pwd(){
+//     return this.password?.get(['pwd']);
+//   }
+
+//   get cpwd(){
+//     return this.password?.get(['cpwd']);
+//   }
+  
+ 
+//   equalPassword (control : AbstractControl) : ValidationErrors | null
+//   {
+
+//     const pwd = control.get('pwd')?.value;
+//     const cpwd = control.get('cpwd')?.value;
+
+//     return pwd ===cpwd ? null : { differentPwd: true };
+//   }
+  
+
+//   onSubmit() {
+
+// }
+
+// }
+  Asignup!:SignUp;
+  AsignupForm = new FormGroup({
+    name: new FormControl('',[Validators.required,Validators.minLength(3), Validators.pattern(/^[A-Za-z]+(?: [A-Za-z]+)*$/)]),
+    emailId: new FormControl('',[Validators.required, Validators.email]),
+    password: new FormControl('',[Validators.required,Validators.minLength(8),Validators.pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)]),
+    conPassword:new FormControl('',[Validators.required,Validators.minLength(8)]),
+    cnumber:new FormControl('',[Validators.required,Validators.pattern(/^[0-9]{10}$/)])
+  })
 
   get name(){
-    return this.signup.get(['name']);
-  }
-  
-  get email(){
-    return this.signup.get(['email']);
+    return this.AsignupForm.get('name');
   }
 
-  get number(){
-    return this.signup.get(['number']);
+  get emailId(){
+    return this.AsignupForm.get('emailId');
   }
 
   get password(){
-    return this.signup.get(['password'])
+    return this.AsignupForm.get('password');
   }
 
-  get pwd(){
-    return this.password?.get(['pwd']);
+  get conPassword(){
+    return this.AsignupForm.get('conPassword');
   }
 
-  get cpwd(){
-    return this.password?.get(['cpwd']);
+  get cnumber(){
+    return this.AsignupForm.get('cnumber');
   }
-  
- 
-  equalPassword (control : AbstractControl) : ValidationErrors | null
-  {
 
-    const pwd = control.get('pwd')?.value;
-    const cpwd = control.get('cpwd')?.value;
+  constructor(private restservice: AuthserviceService){}
 
-    return pwd ===cpwd ? null : { differentPwd: true };
+  onSubmit(){
+    const conpassword = this.AsignupForm.get('conPassword')?.value;
+    const password = this.AsignupForm.get('password')?.value;
+    console.log(conpassword +" " +password)
+    const signUp={
+      UserId:"1234",
+      Name:this.AsignupForm.get('name')?.value??'',
+      Email:this.AsignupForm.get('emailId')?.value??'',
+      Password:this.AsignupForm.get('password')?.value??'',
+      Role:"Agent",
+      ContactNumber:this.AsignupForm.get('cnumber')?.value??'',
+    }
+    if(password!=conpassword){
+      alert("Password not matching with confirm password!!")
+    }else{
+      this.restservice.setCustomerDetails(this.Asignup).subscribe(data =>{
+
+        alert("Account created successfully!");
+      })
+    }
   }
-  
-
-  onSubmit() {
-
 }
 
+export class SignUp{
+  UserId!:string;
+  Name!:string;
+  Email!:string;
+  Password!:string;
+  Role!:string;
+  ContactNumber!:string;
 }
