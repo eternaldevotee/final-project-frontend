828d2ebc3a40fcc67a4aafce3196d1be0c30fe35
Srinivas R <2441319@cognizant.com>
2025-11-06 11:29:49 +0530

    Added features in agent notifications

--- src/app/shared/ui/agent-notifications/agent-notifications.component.ts
+++ src/app/shared/ui/agent-notifications/agent-notifications.component.ts
@@ -15,6 +15,9 @@ export class AgentNotificationsComponent {
   constructor(private shareLoginService : ShareloginService , private agentNotificationService : NotificationServiceService , private router : Router){}
 
   notifications! : AgentNotificationModel[];
+  filteredNotifications! : AgentNotificationModel[];
+  selectedAction : string = '';
+  pkgId! : string;
 
   getTypeColor(type : string) : string {
     switch (type.toLowerCase()) {
@@ -33,10 +36,28 @@ export class AgentNotificationsComponent {
     const adminID = this.shareLoginService.getUserId();
     this.agentNotificationService.getNotifications(adminID).subscribe((data) => {
       this.notifications = data;
+      this.filteredNotifications = data;
     });
   }
   
+  applyFilters() : void {
+    if(!this.selectedAction) {
+      this.filteredNotifications = this.notifications;
+    }
+    else {
+      console.log("hey in the else block")
+      this.filteredNotifications = this.notifications.filter(n => n.type.toLocaleLowerCase() === this.selectedAction.toLocaleLowerCase());
+      console.log(this.filteredNotifications)
+    }
+  }
+
   goBack() : void {
     this.router.navigate(['/agent/home']);
+  }
+
+  filterById() : void {
+    console.log("in filter by id method")
+    this.filteredNotifications = this.notifications.filter(n => n.message.includes(this.pkgId))
+    console.log(this.filteredNotifications)
   }
 }
